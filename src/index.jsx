c323b8d84f2090a3518172bbedbbb48ea1044036
Lucian Binner <lucian.binner@hotmail.com>
2022-06-27 14:28:39 -0300

    feat: ThemeProvider config

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { GlbalStyles } from './styles/global-styles';
+import { theme } from './styles/theme';
 import App from './templates/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <ThemeProvider theme={theme}>
     <App />
     <GlbalStyles />
-  </>,
+  </ThemeProvider>,
 );
