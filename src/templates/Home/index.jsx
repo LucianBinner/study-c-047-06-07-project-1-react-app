8fdb9c67bd13eeaa1d94ebb4a889e2ea9c2654ae
Lucian Binner <lucian.binner@hotmail.com>
2022-07-09 15:18:11 -0300

    feat: loading page by strapi api

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useRef, useState } from 'react';
 import { mapData } from '../../api/map-data';
-import { Base } from '../Base';
-import mock from '../Base/mock';
 import { Container } from './styles';
+
 import { PageNotFound } from '../PageNotFound';
 import { Loading } from '../Loading';
+import { Base } from '../Base';
+
+import { GridTwoColumn } from '../../components/GridTwoColumn';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -46,9 +51,41 @@ const Home = () => {
     return <Loading />;
   }
 
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
   return (
     <Container>
-      <Base {...mock} />
+      <Base
+        links={links}
+        footerHtml={footerHtml}
+        logoData={{
+          text,
+          link,
+          srcImg,
+        }}
+      >
+        {sections.map((section, index) => {
+          const { component } = section;
+          const key = `${slug}-${index}`;
+
+          if (component === 'section.section-two-columns') {
+            return <GridTwoColumn key={key} {...section} />;
+          }
+
+          if (component === 'section.section-contenct') {
+            return <GridContent key={key} {...section} />;
+          }
+
+          if (component === 'section.section-grid-text') {
+            return <GridText key={key} {...section} />;
+          }
+
+          if (component === 'section.section-grid-image') {
+            return <GridImage key={key} {...section} />;
+          }
+        })}
+      </Base>
     </Container>
   );
 };
